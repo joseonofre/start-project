abe9dfefbe3e480d28d934e721bdf6e90b015c35
joseonofre <esoj.neto@gmail.com>
2015-07-26 16:41:26 -0300

    V1.0

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # start-project
 <b>Simples e Rápido</b>
 
-<b>1º</b> sudo npm install -g gulp</br>
-<b>2º</b> sudo npm install</br>
-<b>3º</b> gulp
+<b>1º</b> Baixar o pacote</br>
+<b>2º</b> sudo npm install -g gulp</br>
+<b>3º</b> sudo npm install</br>
+<b>4º</b> gulp
---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * agencia_only
+ * Start project (gulp, materializer e boilerplate) - Agência Only
+ * http://agenciaonly.com.br
+ * @author José Onofre
+ * @version 1.0.0
+ * Copyright 2015. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+
+  });
+
+})(jQuery, window, document);
